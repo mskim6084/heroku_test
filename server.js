33181c5f48d920cd93f65066ceae9ab82e1c5282
MinSung Kim <kimms@myumanitoba.ca>
2022-06-08 15:14:11 -0500

    connecting to heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+app.listen(3000)
